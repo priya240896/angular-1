16104a55ccfbe42bf3d766a5514d1550b7d68cd2
priya240896 <priya.pankhuri96@gmail.com>
2020-02-11 18:47:55 +0530

    5 files modified, 24 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [ AppComponent, HelloComponent,BsNavbarComponent],
+  bootstrap:    [ AppComponent,BsNavbarComponent ]
 })
 export class AppModule { }
---
+++ src/app/check-out/check-out.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'check-out',
+  templateUrl: './check-out.component.html',
+  styleUrls: [ './check-out.component.css' ]
+})
+export class CheckOutComponent implements OnInit  {
+  constructor()
+  {
+  
+  }
+  ngOnInit(){}
+}
