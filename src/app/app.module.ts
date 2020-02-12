08e46932df1c0fc39edd5632a0a6ee03e7c213e8
priya240896 <priya.pankhuri96@gmail.com>
2020-02-12 18:34:22 +0530

    2 files modified, 2 added, 2 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
+import {environment} from './../environments/environment';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import {RouterModule} from '@angular/router'
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap'
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {AngularFireModule} from 'angularfire2';
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+import{AngularFireAuthModule} from 'angularfire2/auth';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
@@ -18,12 +22,11 @@ import { AdminProductsComponent } from './admin/admin-products/admin-products.co
 import { LoginComponent } from './login/login.component';
 
 
-
-
-
-
-@NgModule({
+@NgModule({ 
   imports:      [ BrowserModule, FormsModule,
+  AngularFireModule.initializeApp(environment.firebase),
+  AngularFireDatabaseModule,
+  AngularFireAuthModule,
    NgbModule,
    RouterModule.forRoot([
     {
---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB1e-s8EvgAu-GvEhQLC7xNmTcynxswPk8",
+  authDomain: "angular-cdzsej-c1cbc.firebaseapp.com",
+  databaseURL: "https://angular-cdzsej-c1cbc.firebaseio.com",
+  projectId: "angular-cdzsej-c1cbc",
+  storageBucket: "angular-cdzsej-c1cbc.appspot.com",
+  messagingSenderId: "1089035628953",
+  appId: "1:1089035628953:web:e1721a3459f58c206fa308",
+  measurementId: "G-FDKFKP88VJ"
+  }
+};
