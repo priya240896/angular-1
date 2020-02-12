2b78a6a1269c37603df950c536633a989ddb1df6
priya240896 <priya.pankhuri96@gmail.com>
2020-02-12 16:32:02 +0530

    5 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import {RouterModule} from '@angular/router'
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap'
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
@@ -14,6 +15,7 @@ import { ProductsComponent } from './products/products.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
 import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { LoginComponent } from './login/login.component';
 
 
 
@@ -21,13 +23,23 @@ import { AdminProductsComponent } from './admin/admin-products/admin-products.co
 
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, RouterModule.forRoot([
+  imports:      [ BrowserModule, FormsModule,
+   NgbModule,
+   RouterModule.forRoot([
     {
       path:'', component:HomeComponent},
-     {path:'products', component:ProductsComponent }
+     {path:'products', component:ProductsComponent },
+      { path:'shopping-cart', component:ShoppingCartComponent},
+      { path:'check-out', component:CheckOutComponent},
+      { path:'order-success', component:OrderSuccessComponent},
+      { path:'login', component:LoginComponent},
+       { path:'my-orders', component:MyOrdersComponent},
+      { path:'admin/products', component:AdminProductsComponent},
+      { path:'admin/orders', component:AdminOrdersComponent}
+
   ]) ],
   declarations: [ AppComponent, HelloComponent,BsNavbarComponent,CheckOutComponent,HomeComponent,MyOrdersComponent,
- OrderSuccessComponent,ProductsComponent,ShoppingCartComponent,AdminOrdersComponent,AdminProductsComponent ],
+ OrderSuccessComponent,ProductsComponent,ShoppingCartComponent,AdminOrdersComponent,AdminProductsComponent,LoginComponent ],
   bootstrap:    [ AppComponent,BsNavbarComponent ]
 })
 export class AppModule { }
