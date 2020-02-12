64b8a634e65dd2fdeef878a3908a0018d50f34f7
priya240896 <priya.pankhuri96@gmail.com>
2020-02-12 13:32:54 +0530

    6 files modified, 17 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import {RouterModule} from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
+import { CheckOutComponent } from './check-out/check-out.component';
+import { HomeComponent } from './home/home.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { OrderSuccessComponent } from './order-success/order-success.component';
+import { ProductsComponent } from './products/products.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+
+
+
+
+
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent,BsNavbarComponent],
+  imports:      [ BrowserModule, FormsModule, RouterModule.forRoot([
+    {
+      path:'', component:HomeComponent},
+     {path:'products', component:ProductsComponent }
+  ]) ],
+  declarations: [ AppComponent, HelloComponent,BsNavbarComponent,CheckOutComponent,HomeComponent,MyOrdersComponent,
+ OrderSuccessComponent,ProductsComponent,ShoppingCartComponent,AdminOrdersComponent,AdminProductsComponent ],
   bootstrap:    [ AppComponent,BsNavbarComponent ]
 })
 export class AppModule { }
---
+++ src/app/products/products.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'products',
+  templateUrl: './products.component.html',
+  styleUrls: [ './products.component.css' ]
+})
+export class ProductsComponent implements OnInit  {
+  constructor()
+  {
+  
+  }
+  ngOnInit(){}
+}
