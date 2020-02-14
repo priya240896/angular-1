c502f5e06ec635603d2bd59923d604c5d00b9dd1
priya240896 <priya.pankhuri96@gmail.com>
2020-02-14 14:26:40 +0530

    2 files modified, 2 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFireModule,  } from 'angularfire2';
 
 @Component({
   selector: 'my-app',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,3 @@
-import {environment} from './../environments/environment';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
@@ -24,7 +23,17 @@ import { LoginComponent } from './login/login.component';
 
 @NgModule({ 
   imports:      [ BrowserModule, FormsModule,
-  AngularFireModule.initializeApp(environment.firebase),
+  AngularFireModule.initializeApp({ 
+    apiKey: "AIzaSyCV2nM9AcwyxyTFIEZhCsXo__gyCild5lA",
+  authDomain: "angular-cdzsej-60007.firebaseapp.com",
+  databaseURL: "https://angular-cdzsej-60007.firebaseio.com",
+  projectId: "angular-cdzsej-60007",
+  storageBucket: "angular-cdzsej-60007.appspot.com",
+  messagingSenderId: "324469911241",
+  appId: "1:324469911241:web:be2dabe5ff036bded0a718",
+  measurementId: "G-7RG1KND8GF"
+   
+    }),
   AngularFireDatabaseModule,
   AngularFireAuthModule,
    NgbModule,
